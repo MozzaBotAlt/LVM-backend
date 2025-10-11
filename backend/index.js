4403f85e8229e334f7ccea90bfef8262e6cf22f3
MozzaBotAlt <165459795+MozzaBotAlt@users.noreply.github.com>
2025-10-11 02:27:24 +0000

    updated content

--- backend/index.js
+++ backend/index.js
@@ -8,14 +8,17 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cors({
   origin: '*',
+  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 }))
 
 app.get("/", (req, res) => {
-  res.sendStatus(200);
+  res.sendStatus("Status: ", 200);
 });
 
 app.get("/date", (req, res) => {
-  res.json(new Date().toString());
+  res.json(new Date().toUTCString());
 });
 
 app.get("/data", (req, res) => {
