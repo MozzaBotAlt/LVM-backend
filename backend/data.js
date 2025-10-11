a1fd3d1c721a94ce27eec26f222db9494becac99
MozzaBotAlt <165459795+MozzaBotAlt@users.noreply.github.com>
2025-10-11 01:49:56 +0000

    disabled cors and redone the data api

--- backend/data.js
+++ backend/data.js
@@ -1,5 +1,7 @@
 const data = [
-    { id: 1, name: 'Item 1' },
-]
+  { id: 1, name: "Item 1", value: 100 },
+  { id: 2, name: "Item 2", value: 200 },
+  { id: 3, name: "Item 3", value: 300 },
+];
 
 export default data;
--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,25 @@
-import express from 'express';
-import data from './data.js';
+import express from "express";
+import cors from "cors";
+import data from "./data.js";
 
 const app = express();
-
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
+app.use(express.json());
+app.use(cors({
+  origin: '*',
+}))
+
+app.get("/", (req, res) => {
   res.sendStatus(200);
 });
 
-app.get('/date', (req, res) => {
+app.get("/date", (req, res) => {
   res.send(new Date().toString());
+});
+
+app.get("/data", (req, res) => {
+  res.json(data);
 });
 
 app.listen(PORT, () => {
